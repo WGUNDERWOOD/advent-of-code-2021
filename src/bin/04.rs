3015171ee62d7b31594acc5191c6baab040fab16
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-03-01 14:34:02 -0500

    working on day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,4 +1,69 @@
+const DIM: usize = 5;
+
+fn parse_input(input: &str) -> (Vec<usize>, Vec<Board>) {
+    let mut ls: Vec<&str> = input.lines().collect();
+    let draws: Vec<usize> = ls[0].split(",")
+        .map(|x| x.parse::<usize>().unwrap()).collect();
+
+    ls.push("");
+    let mut boards: Vec<Board> = vec![];
+    let mut numbers: Vec<Vec<usize>> = vec![];
+
+    for i in 2..ls.len() {
+        if ls[i] == "" {
+            let marked = vec![vec![false; DIM]; DIM];
+            let mut board = Board{numbers: numbers.clone(), marked};
+            board.numbers = numbers;
+            boards.push(board);
+            numbers = vec![]
+        } else {
+            let row: Vec<usize> = ls[i].split(" ").filter(|x| !x.is_empty())
+                .map(|x| x.parse::<usize>().unwrap()).collect();
+            numbers.push(row.clone());
+        }
+    }
+    (draws, boards)
+}
+
+#[derive(Debug)]
+struct Board {
+    numbers: Vec<Vec<usize>>,
+    marked: Vec<Vec<bool>>,
+}
+
+impl Board {
+    fn mark(&mut self, n: usize) {
+        for i in 0..DIM {
+            for j in 0..DIM {
+                if self.numbers[i][j] == n {
+                    self.marked[i][j] = true
+                }
+            }
+        }
+    }
+
+    // TODO this is not bingo
+    fn has_won(&self) -> bool {
+        let mut won = true;
+        for i in 0..DIM {
+            for j in 0..DIM {
+                if !self.marked[i][j] {
+                    won = false;
+                }
+            }
+        }
+    won
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let (draws, mut boards) = parse_input(input);
+
+    for n in draws.iter() {
+        for board in &mut boards {
+            board.mark(*n)
+        }
+    }
     None
 }
 
@@ -19,7 +84,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(4512));
     }
 
     #[test]
