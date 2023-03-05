3e2e00d52b0ab8124a04541e7b6a9e88864c6898
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-03-05 14:52:35 -0500

    day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn parse_input(input: &str) -> (Vec<Vec<&str>>, Vec<Vec<&str>>) {
     let lines = input.split('\n').filter(|x| !x.is_empty());
     let mut all_patterns: Vec<Vec<&str>> = vec![];
@@ -10,6 +12,64 @@ fn parse_input(input: &str) -> (Vec<Vec<&str>>, Vec<Vec<&str>>) {
         all_outputs.push(outputs);
     }
     (all_patterns, all_outputs)
+}
+
+fn contains_all(s1: &str, s2: &str) -> bool {
+    let mut ans = true;
+    for c in s2.chars() {
+        if !s1.contains(c) {
+            ans = false;
+        }
+    }
+    ans
+}
+
+fn get_digits<'a>(patterns: &'a Vec<&'a str>) -> HashMap<u32, &'a str> {
+    // Find 1, 4, 7, 8
+    let mut digits: HashMap<u32, &str> = HashMap::new();
+    for pattern in patterns {
+        match pattern.len() {
+            2 => digits.insert(1, pattern),
+            4 => digits.insert(4, pattern),
+            3 => digits.insert(7, pattern),
+            7 => digits.insert(8, pattern),
+            _ => None,
+        };
+    }
+    // Find 0, 3, 6, 9
+    for pattern in patterns {
+        match pattern.len() {
+            5 => {
+                if contains_all(pattern, digits[&1]) {
+                    digits.insert(3, pattern);
+                };
+            }
+            6 => {
+                if !contains_all(pattern, digits[&1]) {
+                    digits.insert(6, pattern);
+                } else if contains_all(pattern, digits[&4]) {
+                    digits.insert(9, pattern);
+                } else {
+                    digits.insert(0, pattern);
+                };
+            }
+            _ => (),
+        };
+    }
+    // Find 2, 5
+    for pattern in patterns {
+        match pattern.len() {
+            5 => {
+                if !contains_all(pattern, digits[&1]) && contains_all(digits[&9], pattern) {
+                    digits.insert(5, pattern);
+                } else if !contains_all(digits[&9], pattern) {
+                    digits.insert(2, pattern);
+                };
+            }
+            _ => (),
+        };
+    }
+    digits
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -26,7 +86,24 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (all_patterns, all_outputs) = parse_input(input);
+    let mut sum_digits = 0;
+
+    for i in 0..all_patterns.len() {
+        let patterns = &all_patterns[i];
+        let outputs = &all_outputs[i];
+        let digits = get_digits(&patterns);
+        let mut outputs_str: String = "".to_string();
+        for output in outputs {
+            for j in 0..=9 {
+                if contains_all(&digits[&j], output) && contains_all(output, &digits[&j]) {
+                    outputs_str.push_str(&j.to_string());
+                }
+            }
+        }
+        sum_digits += outputs_str.parse::<u32>().unwrap();
+    }
+    Some(sum_digits)
 }
 
 fn main() {
@@ -48,6 +125,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 8);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(61229));
     }
 }
