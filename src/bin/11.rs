72240c706a5e84a8f9291ef59ef38a67257aaa32
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-03-08 13:54:24 -0500

    day 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,12 +1,4 @@
-// Approach
-// 1: Add one to every cell
-// 2: repeat until no cells at 10:
-//      for all cells at 10:
-//        cell flashes, increasing neighbors which are < 10 by 1
-//        cell sets to 11 (already flashed state)
-
 use std::cmp::{min, max};
-
 const N: usize = 10;
 
 fn parse_input(input: &str) -> Vec<Vec<u32>> {
@@ -23,7 +15,7 @@ fn parse_input(input: &str) -> Vec<Vec<u32>> {
     octopuses
 }
 
-fn get_neighbors(i: usize, j: usize, octopuses: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
+fn get_neighbors(i: usize, j: usize) -> Vec<(usize, usize)> {
     let mut neighbors: Vec<(usize, usize)> = vec![];
     for ii in max(1,i)-1..=min(N-2,i)+1 {
         for jj in max(1,j)-1..=min(N-2,j)+1 {
@@ -66,27 +58,15 @@ fn add_1(octopuses: &mut Vec<Vec<u32>>) {
 }
 
 fn perform_step(octopuses: &mut Vec<Vec<u32>>) -> u32 {
-
     let mut flashes = 0;
     add_1(octopuses);
-
     while contains_10(&octopuses) {
-
-        //display(&octopuses);
-        //println!("\n");
-
-        // change all 10 to 11
         change_a_to_b(10, 11, octopuses);
-
         for i in 0..N {
             for j in 0..N {
                 if octopuses[i][j] == 11 {
                     flashes += 1;
-                    //println!("\n");
-                    //println!("{:?}", (i, j));
-                    //println!("");
-                    for (ii, jj) in get_neighbors(i, j, octopuses) {
-                        //println!("{:?}", (ii, jj));
+                    for (ii, jj) in get_neighbors(i, j) {
                         if octopuses[ii][jj] < 10 {
                             octopuses[ii][jj] += 1;
                         }
@@ -100,29 +80,38 @@ fn perform_step(octopuses: &mut Vec<Vec<u32>>) -> u32 {
     flashes
 }
 
-fn display(octopuses: &Vec<Vec<u32>>) {
-    for row in octopuses {
-        println!("{:?}", row);
+fn is_simultaneous_flash(octopuses: &Vec<Vec<u32>>) -> bool {
+    let mut flag = true;
+    for i in 0..N {
+        for j in 0..N {
+            if octopuses[i][j] != 0 {
+                flag = false;
+            }
+        }
     }
+    flag
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-
     let mut flashes = 0;
     let mut octopuses = parse_input(input);
     let n_steps = 100;
-
     for _ in 0..n_steps {
         flashes += perform_step(&mut octopuses);
-        //display(&octopuses);
-        //println!("\n");
     }
-
     Some(flashes)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut n_steps = 0;
+    let mut simultaneous_flash = false;
+    let mut octopuses = parse_input(input);
+    while !simultaneous_flash {
+        perform_step(&mut octopuses);
+        simultaneous_flash = is_simultaneous_flash(&octopuses);
+        n_steps += 1;
+    }
+    Some(n_steps)
 }
 
 fn main() {
@@ -144,6 +133,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 11);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(195));
     }
 }
