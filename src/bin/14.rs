934195b6b703e014ab751b57d30f5d604bae4131
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-03-22 15:20:10 -0400

    working on day 14

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,4 +1,49 @@
+use std::collections::HashMap;
+
+fn parse_input(input: &str) -> (String, HashMap<(char, char), char>) {
+    let input_split: Vec<&str> = input.split("\n\n").collect();
+    let polymer = input_split[0].to_string();
+    let rules_list: Vec<&str> = input_split[1].split("\n").
+        filter(|x| !x.is_empty()).collect();
+    let mut rules = HashMap::new();
+
+    for rule in &rules_list {
+        let pair1 = rule.chars().nth(0).unwrap();
+        let pair2 = rule.chars().nth(1).unwrap();
+        let c = rule.chars().nth(6).unwrap();
+        rules.insert((pair1, pair2), c);
+    }
+    return (polymer, rules)
+}
+
+fn iterate_polymer(polymer: &str, rules: &HashMap<(char, char), char>) -> String {
+    let mut new_polymer = "".to_string();
+
+    for i in 0..polymer.len()-1 {
+        let c1 = polymer.chars().nth(i).unwrap();
+        let c2 = polymer.chars().nth(i+1).unwrap();
+
+        new_polymer.push(c1);
+
+        if rules.contains_key(&(c1, c2)) {
+            new_polymer.push(rules[&(c1, c2)]);
+        }
+
+        new_polymer.push(c2);
+    }
+
+    return new_polymer
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let (mut polymer, rules) = parse_input(input);
+    //println!("{:?}", rules);
+    //println!("{:?}", polymer);
+    for rep in 0..8 {
+        println!("{:?}", polymer);
+        polymer = iterate_polymer(&polymer, &rules);
+    }
+        println!("{:?}", polymer.len());
     None
 }
 
@@ -19,7 +64,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 14);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(1588));
     }
 
     #[test]
