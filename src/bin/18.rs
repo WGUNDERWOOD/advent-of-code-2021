f80e6aa979074c6c3681e904193b96a1344ceb3f
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-06-11 12:33:12 -0400

    more work on day 18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,60 +1,85 @@
-struct Snailfish {
-    id: String,
-    left: Option<i32>,
-    right: Option<i32>,
-    value: Option<i32>,
+fn is_int(s: &String) -> bool {
+    let flags: Vec<bool> = s.chars().map(|c| c.is_digit(10)).collect();
+    return !flags.contains(&false);
 }
 
-fn get_middle(s: &str) -> usize {
+fn explode(snailfish: &mut Vec<String>) -> bool {
     let mut bracket_count = 0;
-    let mut middle = 0;
-    for i in 1..s.len()-1 {
-        let c = s.chars().nth(i);
-        if c == Some('[') {bracket_count += 1};
-        if c == Some(']') {bracket_count -= 1};
-        if bracket_count == 0 {
-            middle = i + 1;
+    let mut next_int_location = 0;
+    let mut flag = false;
+
+    // find where to explode
+    let mut explode_comma_location = 0;
+    for i in 0..snailfish.len() {
+        let c = &snailfish[i];
+        if c == "[" {
+            bracket_count += 1
+        };
+        if c == "]" {
+            bracket_count -= 1
+        };
+        if bracket_count >= 5 && is_int(&snailfish[i+1]) {
+            explode_comma_location = i + 2;
+            flag = true;
             break;
         };
-    };
-    return middle
-}
-
-fn parse_snailfish(s: &str) -> Snailfish {
-    // snailfish with no children
-    if s.len() == 3 {
-        let id = s.to_string();
-        let left = None;
-        let right = None;
-        let value = Some(s.chars().nth(1).unwrap().to_digit(10) as i32);
-        return Snailfish{id, left, right, value}
-    } else {
-        let middle = get_middle(s);
-        let mut left: Option<i32> = None;
-        let mut right: Option<i32> = None;
-        let lchar = s.chars().nth(1).unwrap();
-        let rchar = s.chars().nth(middle + 1).unwrap();
-        if lchar.is_digit(10) {
-            left = lchar;
-        } else {
-            left = parse_snailfish(&s[1..middle])
-        }
-
-    dbg!(middle);
-
     }
 
+    // find previous int
+    let mut previous_int_location = 0;
+    for i in 0..explode_comma_location - 1 {
+        if is_int(&snailfish[i]) {
+            previous_int_location = i;
+        };
+    }
 
-    let snailfish = Snailfish{id: "hi", left: None, right: None, value: None};
-    return snailfish
-}
+    // add to previous int
+    if previous_int_location > 0 {
+        let previous_int: i32 = snailfish[previous_int_location].parse().unwrap();
+        let explode_left_int: i32 = snailfish[explode_comma_location - 1].parse().unwrap();
+        let new_previous_int = previous_int + explode_left_int;
+        snailfish[previous_int_location] = new_previous_int.to_string();
+    }
 
-fn parse_snailfishes(input: &str) -> Vec<Snailfish> {
-    return input.split("\n").map(|s| parse_snailfish(s)).collect()
+    // find next int
+    let mut next_int_location = 0;
+    for i in explode_comma_location + 2..snailfish.len() {
+        if is_int(&snailfish[i]) {
+            next_int_location = i;
+            break;
+        };
+    }
+
+    // add to next int
+    if next_int_location > 0 {
+        let next_int: i32 = snailfish[next_int_location].parse().unwrap();
+        let explode_right_int: i32 = snailfish[explode_comma_location + 1].parse().unwrap();
+        let new_next_int = next_int + explode_right_int;
+        snailfish[next_int_location] = new_next_int.to_string();
+    }
+
+    // drop old pair
+    snailfish[explode_comma_location - 1] = "0".to_string();
+    snailfish[explode_comma_location] = "".to_string();
+    snailfish[explode_comma_location + 1] = "".to_string();
+    snailfish.retain(|x| !x.is_empty());
+    return flag;
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let snailfishes = parse_snailfishes(input);
+    let snailfishes: Vec<Vec<String>> = input
+        .split("\n")
+        .map(|x| x.split("").filter(|x| !x.is_empty()).map(|x| x.to_string()).collect())
+        .collect();
+    let mut snailfish = snailfishes[0].clone();
+    //let mut snailfish = "".to_string();
+    //for summand in snailfishes.iter() {
+    //snailfish.push_str(summand);
+    //explode(&mut snailfish);
+    //}
+    dbg!(&snailfish);
+    explode(&mut snailfish);
+    dbg!(snailfish);
     None
 }
 
