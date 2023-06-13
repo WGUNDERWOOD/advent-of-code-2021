bc9d4306c81f36a7e110f35f189dd1c7b9792383
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-06-12 22:01:29 -0400

    cleaning repo

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,4 +1,23 @@
+type Scanner = Vec<Vec<i32>>;
+
+fn parse_scanners(input: &str) -> Vec<Scanner> {
+    let scanners: Vec<Scanner> = input
+        .split("\n\n")
+        .map(|x|
+             x.split("\n")
+             .filter(|x| !x.contains("---"))
+             .map(|x|
+                  x.split(",")
+                  .map(|x| x.parse().unwrap())
+                  .collect())
+             .collect())
+        .collect();
+    return scanners
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let scanners = parse_scanners(input);
+    dbg!(&scanners[0]);
     None
 }
 
@@ -19,7 +38,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 19);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(79));
     }
 
     #[test]
